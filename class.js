4c87fc0bbf605208b54a34c81d7c292bc4f99646
Thibault-Brun <BeNurb@wifi-172-30-18-105.wifi.univ-paris5.fr>
2014-04-22 13:01:22 +0200

    Test 3

--- class.js
+++ class.js
@@ -1,2 +1,14 @@
-// Test 2
-// Test 1
+function PackageDefault (arg1) {
+    this.listActivite =[];
+    this.nom = arg1;
+    this.getInfo = function() {
+        return this.nom;
+    };
+}
+
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    var pack1 = new PackageDefault("toto");
+    console.log(pack1.getInfo());
+});
