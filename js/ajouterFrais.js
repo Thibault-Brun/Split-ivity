9460138bb5150274bf6bf87de1138bd3b28134cf
Martin1011 <martin.76@hotmail.fr>
2014-04-25 12:54:09 +0200

    Ajouter Frais
    Ajouter Frais

--- js/ajouterFrais.js
+++ js/ajouterFrais.js
@@ -1,16 +1,15 @@
 function ajoutFrais(){
 			
-	if ( $.trim($("input[name=nomFrais].editable").first().val())!="" && $.trim($("input[name=prixFrais].editable").first().val())!="" && $.trim($("input[name=qteFrais].editable").first().val())!="") {
+	if ( $.trim($("input[name=nomFrais].editable").first().val()) !="" && ($.trim($("input[name=prixFrais].editable").first().val())!="" && $.trim($("input[name=quantiteFrais].editable").first().val())!="")) {
 			
-		formulaire = '<li class="formFrais">'+$(".formFrais").first().html()+'</li>';
-		alert(formulaire);
-			
-		$("#nomFrais.editable").removeClass("editable");
-		$("#prixFrais.editable").removeClass("editable");
-		$("#qteFrais.editable").removeClass("editable");
-		$("#listeFrais").prepend(formulaire);
+		$(".nomFrais.editable").removeClass("editable");
+		$(".prixFrais.editable").removeClass("editable");
+		$(".quantiteFrais.editable").removeClass("editable");
 		
-		$("input[name=qteFrais].editable").first().focus();
+		
+		$.get("./formulaireFrais.html", function(data){$('#listeFrais').prepend(data);});
+		
+		$("input[name=quantiteFrais].editable").first().focus();
 	}
 }
 
